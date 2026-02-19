ff5df63387dad17afa95a15966e9243a6aa1e86e
xlsft <dev@xlsft.ru>
2026-02-19 14:16:21 +0300

    feat: telegram login button

---
+++ web/app/components/auth/TelegramAuthButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    const props = defineProps<{ id: number }>()
+
+    const emits = defineEmits<{ data: [User] }>()
+    const script = useScript({ src: 'https://telegram.org/js/telegram-widget.js?22', async: true, fetchpriority: 'high' }, { bundle: true, tagPriority: "critical" })
+
+    const click = async () => {
+        await script._loadPromise
+        ;(window as any).Telegram.Login.widgetsOrigin = 'https://oauth.telegram.org'
+        ;(window as any).Telegram.Login.auth({ bot_id: props.id }, (data: any) => {
+            emits('data', data)
+        })
+    }
+</script>
+
+<template>
+    <button @click="click">Войти через Telegram</button>
+</template>
