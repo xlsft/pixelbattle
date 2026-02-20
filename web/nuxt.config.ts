a6dd466d0171dd58fcf4aba88418e4e3c32263d1
xlsft <dev@xlsft.ru>
2026-02-20 13:58:21 +0300

    fix: fonts

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
     css: ['~/assets/css/main.css'],  
-    modules: ['@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt', '@nuxt/scripts'],
+    modules: ['@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt', '@nuxt/scripts', '@nuxt/fonts'],
     ssr: false,
     vite: { 
         plugins: [ tailwindcss() as any ], 
