2bb52fc2600a335aac641654ff18253172621ac8
xlsft <dev@xlsft.ru>
2026-02-21 21:14:01 +0300

    feat: socket reconnect

--- web/app/composables/useBinaryWebSocket.ts
+++ web/app/composables/useBinaryWebSocket.ts
@@ -1,18 +1,35 @@
 type WebsocketOptions = {
-    type: any
+    reconnect?: boolean
 }
 
-export const useBinaryWebSocket = (channel: string, options?: WebsocketOptions) => {
+export const useBinaryWebSocket = (channel: string, options: WebsocketOptions = {}) => {
+    const {
+        reconnect = true
+    } = options
+
     const config = useRuntimeConfig().public
     const base = new URL(config.baseUrl); base.protocol = 'wss:'
-    const ws = new WebSocket(`${base.toString()}api/${channel}`); ws.binaryType = 'arraybuffer'
+
+    let ws: WebSocket | null = null, handler: ((data: Uint8Array) => void) | null = null
+    let timeout: ReturnType<typeof setTimeout> | null = null
+    let closed = false
+
+    const initialize = () => {
+        ws = new WebSocket(`${base.toString()}api/${channel}`); ws.binaryType = 'arraybuffer'
+        ws.onmessage = (e) => { if (!handler) return; handler(new Uint8Array(e.data)) }
+        ws.onclose = () => { if (!reconnect || closed) return; timeout = setTimeout(initialize, 1000)}
+        ws.onerror = () => ws?.close()
+    }; initialize()
+
 
     const actions = {
-        data: (callback: (data: Uint8Array) => void) => ws.onmessage = (e) => {
-            const buffer = new Uint8Array(e.data);
-            callback(buffer)
-        }
+        data: (callback: (data: Uint8Array) => void) => handler = callback,
+        close: () => { closed = true; timeout && clearTimeout(timeout); ws?.close() },
     }
 
-    return { ...actions, ws, base }
+    return {
+        ...actions,
+        get ws() { return ws },
+        base,
+    }
 }
